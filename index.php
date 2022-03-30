5fe61bd194455d809492fddceafc57d140a58307
Ilya-Popov-hub <pop2p.17@gmail.com>
2022-03-30 16:31:46 +0300

    Array sum

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
+$a = [1, 2, 3, 4, 5, 6, 100, null, 4.1];
 
-echo 'Hello1 World';
+function arraySum($array)
+{
+    $sum = 0;
+    foreach ($array as $value) {
+        $sum += $value;
+    }
+    return $sum;
+}
+
+echo 'Значения массива: ' . '<br>';
+var_dump($a);
+echo '<br>';
+$y = arraySum($a);
+echo 'Сумма значений массива: ' . '<br>';
+echo $y . '<br>';
+echo 'Проверка с помощью array_sum(): ' . '<br>';
+echo array_sum($a);
